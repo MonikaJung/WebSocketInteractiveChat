d02725e539e4754ac37ef18f3c52da8d44334093
Monika Jung <m.nika.jung@gmail.com>
2024-05-24 14:51:57 +0200

    refactored index.js code

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-//importing http module
-const http = require('http');
-//importing ws module
-const websocket = require('ws');
-//creating a http server
+// import http and ws modules
+const http = require("http");
+const websocket = require("ws");
+
+// create an http and a websocket servers
 const server = http.createServer((req, res) => {
-res.end("I am connected");
+  res.end("I am connected");
 });
-//creating websocket server
 const wss = new websocket.Server({ server });
-//calling a method 'on' which is available on websocket object
-wss.on('headers', (headers, req) => {
-//logging the header
-console.log('WebSocket.on headers:\n');
-console.log(headers);
+
+// call a method 'on' of websocket object
+wss.on("headers", (headers, req) => {
+  // logg the header to the console
+  console.log("WebSocket.on headers:\n");
+  console.log(headers);
 });
-console.log('Listening on http://localhost:8000 ...');
-//making it listen to port 8000
+console.log("Listening on http://localhost:8000 ...");
+// listen to port 8000
 server.listen(8000);
