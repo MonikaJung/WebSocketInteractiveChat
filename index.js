6313fef9d77118ee15ea719aab0b07e7b9d3e119
Monika Jung <m.nika.jung@gmail.com>
2024-05-24 20:23:07 +0200

    Moved styling to style.css, moved style.css and index.html to public folder, added login page and multiroom chattig option

--- index.js
+++ index.js
@@ -5,13 +5,28 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+app.use(express.static('public'));
+
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+  res.sendFile(__dirname + "public/index.html");
 });
 
 io.on("connection", (socket) => {
-  socket.on("chat message", (msg) => {
-    io.emit("chat message", msg);
+  console.log("new user connected");
+
+  socket.on("join room", ({ username, room }) => {
+    socket.join(room);
+    console.log(`${username} joined ${room}`);
+    socket.to(room).emit('chat message', { user: '[INFO]', text: `${username} has joined the room` });
+    socket.emit('room joined', { message: `You joined ${room}` });
+  });
+
+  socket.on("chat message", ({ text, user, room }) => {
+    io.to(room).emit("chat message", { user: user, text: text });
+  });
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
   });
 });
 
